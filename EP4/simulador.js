e9c54653dfcb490c716b174e5e5a75e9fd2e6550
danigfavero <danifavero@usp.br>
2021-07-20 18:11:59 -0300

    [EP4] prepara arquivos para começar

---
+++ EP4/simulador.js
@@ -0,0 +1,386 @@
+/*
+    EP4 - Simulador de vôo - Parte II
+
+    Autor: Daniela Gonzalez Favero
+    Data: 30 de julho de 2021
+*/
+"use strict";
+
+// canvas
+var canvas;
+var gl;
+
+// animação
+var gInterval;
+
+// câmera
+var modelViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
+var nMatrix, nMatrixLoc;
+var eye;
+
+// Camera position: modelview e projection
+var at = vec3(0.0, 0.0, 0.0);
+var up = vec3(0.0, 1.0, 0.0);
+var radius = 400;  // posição inicial do olho: z=400
+var theta = 0.0;
+var phi = 0.0;
+var dr = 5.0 * Math.PI/180.0;
+
+// iluminação
+var ctm;
+var ambientColor, diffuseColor, specularColor;
+
+// estruturas/buffers
+var positionsArray = [];
+var normalsArray = [];
+var materials = [5.0, 10.0, 20.0, 30.0, 40.0, 100.0];
+
+// constantes
+const DEBUG = true;
+const BG_COLOR = [0.0, 0.0, 0.0, 1.0];
+
+// Valores ASCII
+const [A, C, D, I, J, K, L, O, S, W, X, Z] = [65, 67, 68, 73, 74, 75, 76, 79, 83, 87, 88, 90];
+
+/* ==================================================================
+    Função main
+*/
+window.onload = function main() {
+
+    canvas = document.getElementById("glCanvas");
+
+    gl = canvas.getContext('webgl2');
+    if (!gl) alert("WebGL 2.0 isn't available");
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(BG_COLOR[0], BG_COLOR[1], BG_COLOR[2], BG_COLOR[3]);
+    gl.enable(gl.DEPTH_TEST);
+
+    //  Inicializa os shaders e seus atributos
+    var program = makeProgram(gl, vertexShaderSource, fragmentShaderSource);
+    gl.useProgram(program);
+
+    // Pré-calcula alguns produtos
+    var ambientProducts = [];
+    var diffuseProducts = [];
+    var specularProducts = [];
+
+    for (var i = 0; i < materials.length; i++) {
+        ambientProducts.push(mult(lightAmbient, matAmbient[i]));
+        diffuseProducts.push(mult(lightDiffuse, matDiffuse[i]));
+        specularProducts.push(mult(lightSpecular, matSpecular[i]));
+    }
+
+    // Criando objetos na cena
+    drawOcean();
+    drawIsland();
+
+    // Seta buffers
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+
+    var normalLoc = gl.getAttribLocation(program, "aNormal");
+    gl.vertexAttribPointer(normalLoc, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(normalLoc);
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.STATIC_DRAW);
+
+    // Varyings e uniformes
+    var positionLoc = gl.getAttribLocation( program, "aPosition");
+    gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionLoc);
+
+    modelViewMatrixLoc = gl.getUniformLocation(program, "uModelViewMatrix");
+    projectionMatrixLoc = gl.getUniformLocation(program, "uProjectionMatrix");
+    nMatrixLoc = gl.getUniformLocation(program, "uNormalMatrix");
+
+    gl.uniform4fv(
+        gl.getUniformLocation(program,"uAmbientProduct"),
+        flatten(ambientProducts)
+        );
+    gl.uniform4fv(
+        gl.getUniformLocation(program, "uDiffuseProduct"),
+        flatten(diffuseProducts)
+        );
+    gl.uniform4fv(
+        gl.getUniformLocation(program, "uSpecularProduct"),
+        flatten(specularProducts)
+        );
+    gl.uniform4fv(
+        gl.getUniformLocation(program, "uLightPosition"),
+        flatten(lightPosition)
+        );
+    gl.uniform1fv( 
+        gl.getUniformLocation(program, "uShininess"),
+        matShininess
+        );
+
+    // UI
+    setInterface();
+
+    // Desenha
+    render();
+};
+
+/* ==================================================================
+    Rendering
+*/
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Câmera e perspectiva
+    eye = vec3(
+        radius*Math.sin(theta)*Math.cos(phi),
+        radius*Math.sin(theta)*Math.sin(phi), 
+        radius*Math.cos(theta)
+        );
+
+    modelViewMatrix = lookAt(eye, at, up);
+    projectionMatrix = perspective(fovy, aspect, near, far);
+    nMatrix = normalMatrix(modelViewMatrix, true)
+
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
+    gl.uniformMatrix3fv(nMatrixLoc, false, flatten(nMatrix));
+
+    // Desenha
+    var nOceanVertices = 6;
+    var nIslandVertices = 6 * cena.mapa.length * cena.mapa[0].length;
+    gl.drawArrays(gl.TRIANGLES, 0, nOceanVertices + nIslandVertices);
+    requestAnimationFrame(render);
+}
+
+/* ==================================================================
+    User Interface
+*/
+function setInterface() {
+
+    // play/pause
+    document.getElementById("Jogar").onclick = function(e) {
+        if (e.target.value == "Jogar") { // começa o jogo
+            if (DEBUG) console.log("Começou o jogo!");
+    
+            // html
+            e.target.value = "Pausar";
+    
+            // animação
+            gInterval = setInterval(render, 200);
+    
+        } else { // pausa o jogo
+            if (DEBUG) console.log("Jogo pausado.");
+            
+            // html
+            e.target.value = "Jogar";
+    
+            // animação
+            clearInterval(gInterval);
+        }
+    };
+
+    // passo
+    document.getElementById("Passo").onclick = function() {
+        if (DEBUG) console.log("Passo.");
+        render();
+    };
+
+    document.addEventListener('keydown', e => {
+        if (DEBUG) console.log("Tecla pressionada: ", e.key, ", Code:", e.keyCode);
+
+        switch (e.keyCode) {
+
+            // translação
+            case J:
+                console.log("Decrementa velocidade");
+                break;
+
+            case L:
+                console.log("Incrementa velocidade");
+                break;
+
+            case K:
+                console.log("Zera velocidade");
+                break;
+
+            // câmera
+            case I:
+                console.log("Aproxima câmera");
+                break;
+
+            case O:
+                console.log("Afasta câmera");
+                break;
+
+            // rotação
+            // em x
+            case W:
+                console.log("Incrementa rotação em x");
+                break;
+
+            case X:
+                console.log("Decrementa rotação em x");
+                break;
+
+            // em y
+            case A:
+                console.log("Incrementa rotação em y");
+                break;
+
+            case D:
+                console.log("Decrementa rotação em y");
+                break;
+
+            // em z
+            case Z:
+                console.log("Incrementa rotação em z");
+                break;
+
+            case C:
+                console.log("Decrementa rotação em z");
+                break;
+
+            // zera
+            case S:
+                console.log("Zera velocidades de rotação");
+        }
+    });
+}
+
+/* ==================================================================
+    Shaders
+*/
+var vertexShaderSource = `#version 300 es
+
+in vec4 aPosition;
+in vec4 aNormal;
+out vec3 N, L, E;
+flat out int matIndex;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+uniform vec4 uLightPosition;
+uniform mat3 uNormalMatrix;
+
+void main() {
+
+    // ajustes da câmera e iluminação
+    vec3 light;
+    vec3 pos = (uModelViewMatrix * aPosition).xyz;
+
+    L = - normalize(uLightPosition.xyz);
+    E = normalize(pos);
+    N = normalize(uNormalMatrix * aNormal.xyz);
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aPosition;
+
+    // define cor de cada material
+    int i;
+    float materials[6] = float[6](5.0, 10.0, 20.0, 30.0, 40.0, 100.0);
+    for (i = 0; i < 6; i++) {
+        if (aPosition.z < materials[i]) {
+            break;
+        }
+    }
+    matIndex = i;
+}
+`
+
+var fragmentShaderSource = `#version 300 es
+
+precision mediump float;
+
+uniform vec4 uAmbientProduct[6];
+uniform vec4 uDiffuseProduct[6];
+uniform vec4 uSpecularProduct[6];
+uniform float uShininess[6];
+
+in vec3 N, L, E;
+flat in int matIndex;
+out vec4 fColor;
+
+void main() {
+
+    vec3 H = normalize(L + E);
+    vec4 ambient = uAmbientProduct[matIndex];
+
+    float Kd = max(dot(L, N), 0.0);
+    vec4 diffuse = Kd * uDiffuseProduct[matIndex];
+
+    float Ks = pow(max(dot(N, H), 0.0), uShininess[matIndex]);
+    vec4 specular =  Ks * uSpecularProduct[matIndex];
+
+    if (dot(L, N) < 0.0) {
+        specular = vec4(0.0, 0.0, 0.0, 1.0);
+    }
+
+    fColor = ambient + diffuse + specular;
+    fColor.a = 1.0;
+}
+`
+
+/* ==================================================================
+    Funções para desenhar primitivas geométricas
+*/
+// desenha retângulo:
+// recebe 4 vértices de uma face
+// monta os dois triângulos voltados para "fora"
+function rect(a, b, c, d) {
+    triangle(a, b, c);
+    triangle(a, c, d);
+}
+
+// desenha triângulo:
+// recebe 3 vértices de um triângulo
+// monta o triângulo voltado para "fora"
+function triangle(a, b, c) {
+    var t1 = subtract(b, a);
+    var t2 = subtract(c, a);
+    var normal = normalize(cross(t2, t1));
+    normal = vec4(normal[0], normal[1], normal[2], 0.0);
+
+    normalsArray.push(normal);
+    normalsArray.push(normal);
+    normalsArray.push(normal);
+
+    positionsArray.push(a);
+    positionsArray.push(b);
+    positionsArray.push(c);
+}
+
+/* ==================================================================
+    Funções de criação dos elementos da cena
+*/
+// desenha o oceano com as coordenadas disponíveis no arquivo config.js
+function drawOcean() {
+    var [xmin, ymin, xmax, ymax] = cena.oceano;
+    var a = vec4(xmin, ymax, 0.0, 1.0);
+    var b = vec4(xmin, ymin, 0.0, 1.0);
+    var c = vec4(xmax, ymin, 0.0, 1.0);
+    var d = vec4(xmax, ymax, 0.0, 1.0);
+
+    rect(a, b, c, d);
+    if (DEBUG) console.log("Oceano criado dentro dos limites: ", 
+                            [xmin, ymin, xmax, ymax] );
+}
+
+// desenha a ilha com as coordenadas disponíveis no arquivo config.js
+function drawIsland() {
+    var [xmin, ymin, xmax, ymax] = cena.ilha;
+
+    for (var x = xmin; x < xmax-1; x++) {
+        for (var y = ymin; y < ymax-1; y++) {
+            var [i, j] = [x + 50, y + 50];
+
+            var a = vec4(  x, y+1,   cena.mapa[i][j+1], 1.0);
+            var b = vec4(  x,   y,     cena.mapa[i][j], 1.0);
+            var c = vec4(x+1,   y,   cena.mapa[i+1][j], 1.0);
+            var d = vec4(x+1, y+1, cena.mapa[i+1][j+1], 1.0);
+
+            rect(a, b, c, d);
+        }
+    }
+    if (DEBUG) console.log("Ilha criada dentro dos limites: ", 
+                            [xmin, ymin, xmax, ymax] );
+}
---
+++ EP4/webglUtils.js
@@ -0,0 +1,55 @@
+/*
+    EP4 - Simulador de vôo - Parte II
+
+    WebGL Utils contem funções auxiliares que devem ajudar
+    você a criar os shaders e programar com WebGL.
+*/
+
+// make program  => compile and link shaders
+
+function makeProgram(gl, vertexShaderSrc, fragmentShaderSrc) 
+{
+    // Compilar e linkar os shaders
+    var vertexShader = compile(gl, gl.VERTEX_SHADER, vertexShaderSrc);
+    var fragmentShader = compile(gl, gl.FRAGMENT_SHADER, fragmentShaderSrc);
+    var program = link(gl, vertexShader, fragmentShader);
+    if (program) {
+        return program;
+    }
+    alert("ERRO: na criação do programa.");
+};
+
+// ========================================================
+// Compile shaders
+function compile (gl, type, source) 
+{
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var deuCerto = gl.getShaderParameter(shader, gl.COMPILE_STATUS); 
+    if (deuCerto) {
+        return shader;
+    }
+    // mostra o erro
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader); // cleanup
+
+};
+
+// ========================================================
+// Link Shaders
+function link (gl, vertexShader, fragmentShader) 
+{
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    var deuCerto = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (deuCerto) {
+        return program;
+    }
+
+    console.log(gl.ProgramInfoLog(program));
+    gl.deleteProgram(program);
+};
